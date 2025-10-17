a6e2af4b106c7fcd9a3aef386d6e063451d5a6a2
Abhay Prajapati <prajapatiabhay.p22@gmail.com>
2025-10-17 09:56:50 +0530

    refactor: convert apiEndpoints from TypeScript to JavaScript

---
+++ utils/api/apiEndpoints.js
@@ -0,0 +1,14 @@
+class apiEndpoints {
+  static base = '/api';
+
+  //profile
+  static profile = `${this.base}/profile`;
+  
+  //contact
+  static contact = `${this.base}/contact`;
+
+  //articles
+  static articles = `${this.base}/articles`;
+}
+
+export default apiEndpoints;
